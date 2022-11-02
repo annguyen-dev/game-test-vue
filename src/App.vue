a291534da7595239e36a730855269eeda68632f9
annguyen <annguyen.dev0594@gmail.com>
2022-11-02 10:42:28 +0700

    create app game

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    <HeaderComponent />
+    <vue-page-transition name="zoom">
+      <router-view @pick="pick = $event" :pickSelected="this.pick" />
+    </vue-page-transition>
+    <FooterComponent />
+  </div>
+</template>
+
+<script>
+import HeaderComponent from "./components/HeaderComponent.vue";
+import FooterComponent from "./components/FooterComponent.vue";
+
+export default {
+  name: "App",
+  components: {
+    HeaderComponent,
+    FooterComponent,
+  },
+  data() {
+    return {
+      pick: null,
+    };
+  },
+  // methods: {
+  //   getEvent(el) {
+  //     console.log("el", el);
+  //   },
+  // },
+};
+</script>
+
+<style>
+@import "./assets/css/styles.css";
+</style>
---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,31 @@
+<template>
+  <header id="header">
+    <div class="logo">
+      <img
+        id="classic-logo"
+        src="../assets/images/logo.svg"
+        alt="Rock, Paper, Scissors"
+      />
+      <!-- <img
+        id="extended-logo"
+        src="../assets/images/logo-bonus.svg"
+        alt="Rock, Paper, Scissors, Lizard, Spock3" -->
+      />
+    </div>
+
+    <div id="score-box">
+      <span class="score-text">Score</span>
+
+      <span id="score" class="classic-score">{{ $store.state.counter }}</span>
+      <span id="score" class="extended-score">{{
+        $store.state.extendedCounter
+      }}</span>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "HeaderComponent",
+};
+</script>
